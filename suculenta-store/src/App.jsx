2880090371f087b699130a7c033cbef4f63c07be
marciolou <marcioalbertoo.lourenco@gmail.com>
2023-12-29 17:32:04 -0300

    feat: Adicionando rotas, header e footer

--- suculenta-store/src/App.jsx
+++ suculenta-store/src/App.jsx
@@ -1,8 +1,10 @@
+import Rotas from './components/Routs/Routs';
 import './style/Global.css';
 
 function App() {
   return (
     <div className="App">
+      <Rotas />
     </div>
   );
 }
---
+++ suculenta-store/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Rodape from '../footer/Footer';
+import Header from '../header/Header';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Header />
+        <Rodape />
+    </div>
+);
+
+export default HomePage;
