dcc3b116191917265747bfb0dee342142e745830
marciolou <marcioalbertoo.lourenco@gmail.com>
2024-01-04 15:11:22 -0300

    feat: adicionando sections na homepage

--- suculenta-store/src/components/HomePage/HomePage.jsx
+++ suculenta-store/src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Rodape from '../footer/Footer';
 import Header from '../header/Header';
+import SectionOne from './Section1/SectionOne';
 
 const HomePage = () => (
     <div className='homepage'>
         <Header />
+        <SectionOne />
         <Rodape />
     </div>
 );
