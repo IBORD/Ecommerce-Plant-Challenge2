7013e7766596c5a9e1d676d6e50021061611d8f4
marciolou <marcioalbertoo.lourenco@gmail.com>
2024-01-05 17:29:27 -0300

    style: adicionando estilos da Product Page

--- suculenta-store/src/components/ProductPage/ProductPage.jsx
+++ suculenta-store/src/components/ProductPage/ProductPage.jsx
@@ -1,19 +1,25 @@
 import Cacto from '../../assets/cactus.svg';
+import Rodape from '../footer/Footer';
 import Header from '../header/Header';
-import Rodape from '../footer/Footer'
+import './ProductPage.css';
 
 const ProductPage = () => {
- 
+
   return (
-    
-    <section>
+    <section className='productpage'>
       <Header />
-      <div>
+      <div className='product_container'>
         <img src={Cacto} alt="cacto" />
-        <div>
+        <div className='description_plant'>
           <h1>Echinocereus Cactus</h1>
           <h2> A Majestic Addition to Your Plant Collection</h2>
-          <div>
+          <div className='options'>
+            <p>indoor</p>
+            <p>cactus</p>
+          </div>
+          <p>$139.99</p>
+          <button className='checkout'>Check out</button>
+          <div className='features'>
             <h3>Features</h3>
             <ul>
               <li>Species: Echinocereus spp.</li>
@@ -22,7 +28,7 @@ const ProductPage = () => {
               <li>Pot Size: Available in various pot sizes to suit your preference and needs.</li>
             </ul>
             <h3>Description</h3>
-            <p>Ladyfinger cactus (*Echinocereus pentalophus*) is also known as Alice, Devil's Fingers, and Dog Tail. It needs bright sunlight, light fertilizer, and is prone to root rot. The root system is shallow and weak. Aphids and mealybugs are also a danger. Avoiding wet soil can help with success with a ladyfinger cactus.</p>
+            <p className='p'>Ladyfinger cactus (*Echinocereus pentalophus*) is also known as Alice, Devil's Fingers, and Dog Tail. It needs bright sunlight, light fertilizer, and is prone to root rot. The root system is shallow and weak. Aphids and mealybugs are also a danger. Avoiding wet soil can help with success with a ladyfinger cactus.</p>
           </div>
         </div>
       </div>
